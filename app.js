ab07e83f0527bb179e70b7cc7ce168384083a4b1
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-03-09 13:02:10 -0800

    edited Express routing parameters and edited app.js.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+let posts = [];
 
 app.set('view engine', 'ejs');
 
@@ -16,7 +17,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get('/',function(req,res) {
-  res.render('home' , {startingContent : homeStartingContent});
+  res.render('home' , {startingContent : homeStartingContent,
+  postList: posts});
 });
 
 app.get('/about',function(req,res) {
@@ -28,12 +30,20 @@ app.get('/contact',function(req,res) {
 });
 
 app.get('/compose',function(req,res) {
-  res.render('compose' ,{} );
+  res.render('compose');
+});
+
+app.get('/posts/:postName',function(req,res) {
+  console.log(req.params.postName);
 });
 
 app.post('/compose',function(req,res) {
-  console.log(req.body.postTitle);
-  // res.render('compose' ,{} );
+  const post = {
+    title: req.body.postTitle,
+    body: req.body.postBody
+  };
+  posts.push(post);
+  res.redirect('/');
 });
 
 
