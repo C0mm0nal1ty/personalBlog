5305e8e7fafd79b1b831f47cf43af09e8ee05a6b
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-03-09 11:12:54 -0800

    changed header.ejs

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get('/',function(req,res) {
-  res.render('home' , {startingContent : homeStartingContent});
+  res.render('home' , {startingContent : homeStartingContent, home: "home"});
 });
 
 app.get('/about',function(req,res) {
